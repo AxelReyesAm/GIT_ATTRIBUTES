bc42f1f6f347f25f678e15908f0a92173751b0d3
careyes@ikeasistencia.com <careyes@ikeasistencia.com>
2021-12-08 15:51:53 -0600

    dev values

--- src/java/constants.java
+++ src/java/constants.java
@@ -3,5 +3,5 @@ public class Constants {
     public static final String PASSWORD ="12345";
     public static final String JDBCURL ="172.20.50.11";
     public static final String DB ="IKE_DEV";
-    public static final String KEY ="QQWEWQERTRYRTGFHHF";
+    public static final String KEY ="QAAAAAAAA";
 }

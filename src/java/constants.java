aea1ac23031096cb36498ea9ca44c672e43b6755
careyes@ikeasistencia.com <careyes@ikeasistencia.com>
2021-12-08 15:49:35 -0600

    prod values

--- src/java/constants.java
+++ src/java/constants.java
@@ -1,7 +1,6 @@
 public class Constants {
-    public static final String USER ="DEV";
-    public static final String PASSWORD ="12345";
-    public static final String JDBCURL ="172.20.50.11";
-    public static final String DB ="IKE_DEV";
-    public static final String KEY ="QQWEWQERTRYRTGFHHF";
+    public static final String USER ="PROD";
+    public static final String PASSWORD ="1111";
+    public static final String JDBCURL ="172.11.100.199";
+    public static final String DB ="IKE_PROD";
 }

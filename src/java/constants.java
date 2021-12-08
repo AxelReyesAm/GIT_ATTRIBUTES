ca9f40a021fb8e221c5cd5080aa59b81774530af
careyes@ikeasistencia.com <careyes@ikeasistencia.com>
2021-12-08 16:07:12 -0600

    qa values

--- src/java/constants.java
+++ src/java/constants.java
@@ -1,6 +1,6 @@
 public class Constants {
-    public static final String USER ="PROD";
-    public static final String PASSWORD ="1111";
-    public static final String JDBCURL ="172.11.100.199";
-    public static final String DB ="IKE_PROD";
+    public static final String USER ="QA";
+    public static final String PASSWORD ="888888";
+    public static final String JDBCURL ="172.88.10.250";
+    public static final String DB ="IKE_QA";
 }
